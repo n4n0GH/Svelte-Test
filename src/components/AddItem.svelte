734ececbb2aa77980ecbf1d1b4c1975d713f59f2
n4n0GH <n4n0@tuta.io>
2021-05-07 21:27:05 +0200

    created mvp todo app

---
+++ src/components/AddItem.svelte
@@ -0,0 +1,34 @@
+<div class="input-group">
+	<input 
+		type="text" 
+		class="form-control" 
+		placeholder="Remember the milk" 
+		bind:value={todoInput}
+		on:keypress={addWithReturn}
+	/>
+	<button 
+		class="btn btn-outline-success"
+		on:click={addItem}
+	>
+		{buttonLabel}
+	</button>
+</div>
+
+<script>
+	import { createEventDispatcher } from 'svelte'
+
+	export let buttonLabel
+	const dispatch = createEventDispatcher()
+	let todoInput = null
+
+	function addItem () {
+		dispatch('addItem', {
+			value: todoInput
+		})
+		todoInput = null
+	}
+
+	function addWithReturn (event) {
+		if (event.charCode === 13) addItem()
+	}
+</script>
---
+++ src/components/TodoItem.svelte
@@ -0,0 +1,25 @@
+<div class="form-check todo-item ml-2 mt-4">
+	<input 
+		class="form-check-input" 
+		id={todoKey} 
+		type="checkbox" 
+		bind:checked={status}
+	/>
+	<label for={todoKey}>
+		{todoItem}
+	</label>
+</div>
+
+<script>
+	import { createEventDispatcher } from 'svelte'
+	const dispatch = createEventDispatcher()
+
+	export let todoItem
+	export let todoKey
+
+	let status = false
+
+	$: if (status) dispatch('done', {
+		key: todoKey
+	})
+</script>
